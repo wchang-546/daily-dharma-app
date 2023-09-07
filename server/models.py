49de81eb7e98971ffaeaee2ef340ac4b1d6d7465
Will Chang <william.chaang@gmail.com>
2023-09-07 16:21:14 -0700

    Initial database models created. No relationship between prompts and users, I believe.

--- server/models.py
+++ server/models.py
@@ -12,7 +12,8 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String) 
-
+    
+    calender_entries = db.relationship('CalendarEntry', backref='user') 
     journal_entries = db.relationship('JournalEntry', backref='user')
 
     @hybrid_property
@@ -31,4 +32,37 @@ class User(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f'<User {self.username}>'
+
+class CalendarEntry(db.Model, SerializerMixin):
+    __tablename__ = 'calendar_entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    mood = db.Column(db.String) 
+    created_date = db.Column(db.DateTime, server_default=db.func.now())
+    updated_date = db.Column(db.DateTime, on_update=db.func.now())
+
+class JournalEntry(db.Model, SerializerMixin):
+    __tablename__ = 'journal_entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    entry = db.Column(db.String)
+    #Make sure prompt is included in the POST request 
+    prompt = db.Column(db.String)
+   
+    created_date = db.Column(db.DateTime, server_default=db.func.now())
+    updated_date = db.Column(db.DateTime, on_update=db.func.now())
+
+class CareerJournalPrompt(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String) 
+
+class SelfGrowthJournalPrompt(db.Model, SerializerMixin): 
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String)
+
+class RelationshipJournalPrompt(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String) 
 
